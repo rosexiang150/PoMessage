1f0ea14082caeec5644b1e0078d40c7087e2165b
rosexiang150 <2248011149@qq.com>
2020-04-04 11:13:52 +0800

    test文件给方法做标记，如不执行这个方法，跳过这个方法，给方法做顺序等。

--- scripts/test_message.py
+++ scripts/test_message.py
@@ -1,12 +1,15 @@
 import os, sys
 sys.path.append(os.getcwd())
 
+import allure
+import pytest
+
+
+
 from page.sendMessage_page import SendMessagePage
 from base.base_driver import BaseDriver
 
 class TestAddPerson:
-
-
 
     def setup(self):
         baseDriver = BaseDriver()
@@ -15,18 +18,19 @@ class TestAddPerson:
         self.sendMessage.click_add_people()
         self.sendMessage.click_all_button()
 
-
-    #发短信
-    def test_send_mes(self):
-        self.sendMessage.click_selected_button()
-        self.sendMessage.input_text_button('hellodoyoudddddd')
-        self.sendMessage.click_send_button()
-
-    #打电话
+    #打电话 allure的pytest
+    # @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @pytest.mark.skipif(condition=2 > 1, reason="跳过该函数")
+    @allure.step('我是测试步骤001')
     def test_call_person(self):
         self.sendMessage.click_img_button()
         self.sendMessage.click_phone_button()
 
-
-
-
+    # 发短信---pytest
+    # @pytest.mark.run(order=2)
+    # @pytest.mark.xfail(2 > 1, reason="标注为预期失败")
+    # @pytest.mark.parametrize('contents', ['aa', 'bb', 'cc', 'dd'])
+    def test_send_mes(self):
+        self.sendMessage.click_selected_button()
+        self.sendMessage.input_text_button('contentscontents')
+        self.sendMessage.click_send_button()
